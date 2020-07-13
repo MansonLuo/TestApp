8079349f2126a58ce54a8cc558cb2ba409a69040
MansonLuo <1558911620@qq.com>
2020-07-13 11:25:53 +0800

    test

--- app/src/main/java/com/mycompany/testapp/MainActivity.java
+++ app/src/main/java/com/mycompany/testapp/MainActivity.java
@@ -2,17 +2,24 @@ package com.mycompany.testapp;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
+	private EditText input;
+	private Button send;
+	private TextView show;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		input = (EditText)findViewById(R.id.input);
+		send = (Button)findViewById(R.id.send);
+		show = (TextView)findViewById(R.id.show);
     }
 
-    public void test() {
-	    System.out.println("test");
-    }
+   
 }
